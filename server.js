5fafdd2bd806dcfafbcd482fa4df32d4d5b23054
sambam144 <slidgard@gmail.com>
2025-11-02 09:31:42 -0600

    ADD LICENSE TO PROJECT FILES

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+// GM Screen - CandleOS
+// Copyright (C) 2025 Sam Lidgard
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// See the file LICENSE for details.
 // Load our secret key from the .env file
 require('dotenv').config();
 const express = require('express');
